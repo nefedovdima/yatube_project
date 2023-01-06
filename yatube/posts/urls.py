525e994b5559c762aa0674fb441be35e1fce75fa
Нефедов Дмитрий <nefedovdl@mail.ru>
2023-01-06 15:44:49 +0300

    Добавляется возможность переходить между страницами с помощью url-ссылок и namespace

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
 
+# Эта строчка обязательна. 
+# Без неё namespace работать не будет:
+# namespace должен быть объявлен при include и тут, в app_name
+app_name = 'posts'
+
 urlpatterns = [
     # Главная страница
-    path('', views.index),
+    path('', views.index, name='index'),
     # Страница со групп постов
-    path('group_list/', views.group_posts),
+    path('group_list/', views.group_posts, name='group_posts'),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,13 +5,23 @@ from django.http import HttpResponse
 
 from django.shortcuts import render
 
+
+
+
+
 # Главная страница
 def index(request):
+    context = {
+    'title' : 'Это главная страница проекта Yatube',
+    }
     template = 'posts/index.html'
-    return render(request, template) 
+    return render(request, template, context) 
 
 
 # Страница со списком посты
 def group_posts(request):
+    context = {
+    'title' : 'Здесь будет информация о группах проекта Yatube'
+    }
     template = 'posts/group_list.html'
-    return render(request, template)
+    return render(request, template, context)
